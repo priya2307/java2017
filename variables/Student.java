e8de3cebd09bca6f187b7a5a61321e58f3310cac
priya2307 <priyaalli.sep7@gmail.com>
2017-06-15 17:33:34 -0400

    First program local variable

---
+++ variables/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public void priya() 
+{
+int id=101;
+String sub="lean";
+System.out.println("priya id is=" +id);
+System.out.println("priya subject is=" +sub);
+}
+public static void main (String args[]){
+Student s1= new Student();
+s1.priya();
+}
+}
