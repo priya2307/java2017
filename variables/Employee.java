1888edbdabc8cf922320c9144fb27ff81167011c
priya2307 <priyaalli.sep7@gmail.com>
2017-06-17 17:50:30 -0400

    Instance Varible

---
+++ variables/Employee.java
@@ -0,0 +1,16 @@
+public class Employee
+{
+String name;
+Byte id;
+public void priya()
+{
+System.out.println("Name="+name);
+System.out.println("ID="+id);
+}
+public static void main (String args[])
+{
+Employee e1=new Employee();
+e1.name="priya";
+e1.id=50;
+}
+}
