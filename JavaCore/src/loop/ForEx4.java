d4ee3a1b87999e7aa08bc29f09b91812c63043ad
priya2307 <priyaalli.sep7@gmail.com>
2017-07-10 17:52:03 -0400

    continue

---
+++ JavaCore/src/loop/ForEx4.java
@@ -0,0 +1,20 @@
+package loop;
+//using continue statement
+public class ForEx4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+	    {
+				if(i==5)
+				{
+					continue;				
+				}
+				System.out.println(" Print = "+i);
+		}	
+
+	}
+
+}
---
+++ JavaCore/src/loop/ForEx5.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class ForEx5 {
+	public static void main(String[] args) 
+	        {
+		        for(int i=1;i<=5;i++)
+			        {
+			 		for(int j=1;j<=3;j++)
+			 			{
+			 			if(j==1)
+			 				{
+			 				continue;
+			 				}
+				         System.out.println("Priya");
+                          }
+				    System.out.println("I");	
+ 
+			        }			 		
+
+              }
+         }
