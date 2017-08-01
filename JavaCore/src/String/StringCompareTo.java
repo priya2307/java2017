a5197c066e9d4e179944bafea41ca4a6616f7b2d
priya2307 <priyaalli.sep7@gmail.com>
2017-07-31 20:27:18 -0400

    Compareto

--- JavaCore/src/String/StringCompareTo.java
+++ JavaCore/src/String/StringCompareTo.java
@@ -3,13 +3,14 @@ package String;
 public class StringCompareTo {
 
 	public static void main(String[] args) {
-	      String str = "Hello World";
-	      String anotherString = "hello world";
-	      Object objStr = str;
-
-	      System.out.println( str.compareTo(anotherString) );
-	      System.out.println( str.compareToIgnoreCase(anotherString) );
-	      System.out.println( str.compareTo(objStr.toString()));
+		String p1= "PRIYA";
+		String p2= new String("Alli");
+		String p3=new String("RAHUL");
+		
+		int result = p1.compareTo(p2);
+		int result2=p1.compareTo(p3);
+		System.out.println("result  =" +result);
+		System.out.println("result2  =" +result2);
 
 	}
 
